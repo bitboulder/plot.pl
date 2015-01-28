c26357b200951d591ee4b42222363802fd918f02
Frank Duckhorn <frank.duckhorn@ikts-md.fraunhofer.de>
2015-01-28 09:46:33 +0100

    offset for tics

--- plot.pl
+++ plot.pl
@@ -237,9 +237,10 @@ sub usage {
 	print "                     by obmitting label (P:), the positions are used\n";
 	print "                     prefix a specifies to add labels, r to replace (default is first replace than add)\n";
 	print "                     to put a \",\" in the label you can use \";;\"\n";
-	print "  -[xy(cb)]2?tics[ar]? S:I:E | S:I/R:E\n";
+	print "  -[xy(cb)]2?tics[ar]? S:I:E | S:I/R[+-]O?:E\n";
 	print "                     places labels beginning at position S with increment I up to E\n";
-	print "                     with R the position is multiplied with R [@]\n";
+	print "                     with R the position is multiplied with R\n";
+	print "                     with O the position is offset [@]\n";
 	print "  -[xy(cb)]2?label TXT label for x/y/x2/y2-axis [@]\n";
 	print "  -[xy]grid          [xy]-axis grid [@]\n";
 	print "  -size W,H          set drawing size for gnuplot\n";
@@ -410,9 +411,9 @@ sub readtics {
 	my @gp=();
 	my $i=0;
 	$name.=" add" if "a"eq$typ;
-	if($tics=~/^(-?[0-9.]*):([0-9.]+)(\/([0-9.]+))?:(-?[0-9.]*)$/){
-		(my $s,my $i,my $r,my $e)=($1,$2,$4,$5);
-		if(""eq$r){
+	if($tics=~/^(-?[0-9.]*):([0-9.]+)(\/([0-9.]+))?([+-][0-9.]+):(-?[0-9.]*)$/){
+		(my $s,my $i,my $r,my $o,my $e)=($1,$2,$4,$5,$6);
+		if(""eq$r && ""eq$o){
 			my $res=$i;
 			die "label-end set but start obmitted for $name" if ""eq$s && ""ne$e;
 			$res=$s.",".$res if ""ne$s;
@@ -420,8 +421,10 @@ sub readtics {
 			return "set ".$name." ".$res."\n";
 		}else{
 			die "label-rate set but start,inc or end obmitted for $name" if ""eq$s || ""eq$i || ""eq$e;
+			$r=1 if ""eq$r;
+			$o=0 if ""eq$o;
 			my @res=();
-			for(my $v=$s;$v<=$e;$v+=$i){ push @res,"\"$v\" ".($v*$r); }
+			for(my $v=$s;$v<=$e;$v+=$i){ push @res,"\"$v\" ".($v*$r+$o); }
 			return "set ".$name." ( ".(join ",",@res)." )\n";
 		}
 	}else{
