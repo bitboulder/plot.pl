af5f75c80ea0f2b588da1b950b8098c474eb2ee3
Frank Duckhorn <frank.duckhorn@ikts-md.fraunhofer.de>
2015-06-29 11:29:40 +0200

    fix gnuplot path

--- plot_arg.cmd
+++ plot_arg.cmd
@@ -3,7 +3,7 @@
 set INT=%USERPROFILE%\loc\int
 set PLOT=%INT%\plot.pl\plot.pl
 set PATH=%PATH%;%INT%\mingw\msys\1.0\bin
-set PATH=%PATH%;%INT%\gnuplot\gp465-win32\bin
+set PATH=%PATH%;%INT%\gnuplot\gnuplot\bin
 
 perl.exe %PLOT% %1
 
