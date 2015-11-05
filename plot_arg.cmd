5b2e26ba6288d83e2c2c1e5b54d5da93944b4a3a
Frank Duckhorn <frank.duckhorn@ikts-md.fraunhofer.de>
2015-11-05 10:53:26 +0100

    switch from mingw to cygwin perl

--- plot_arg.cmd
+++ plot_arg.cmd
@@ -2,7 +2,8 @@
 
 set INT=%USERPROFILE%\loc\int
 set PLOT=%INT%\plot.pl\plot.pl
-set PATH=%PATH%;%INT%\mingw\msys\1.0\bin
+REM set PATH=%PATH%;%INT%\mingw\msys\1.0\bin
+set PATH=%PATH%;%INT%\cygwin\cygwin64\bin
 set PATH=%PATH%;%INT%\gnuplot\gnuplot\bin
 
 perl.exe %PLOT% %1
