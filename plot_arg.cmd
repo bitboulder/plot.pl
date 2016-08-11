c2f3cb7d16f2713378413254737a30c1e443658d
Frank Duckhorn <frank.duckhorn@ikts-md.fraunhofer.de>
2016-08-11 10:40:36 +0200

    use non-cygwin gnuplot

--- plot_arg.cmd
+++ plot_arg.cmd
@@ -4,7 +4,7 @@ set INT=%USERPROFILE%\loc\int
 set PLOT=%INT%\plot.pl\plot.pl
 REM set PATH=%PATH%;%INT%\mingw\msys\1.0\bin
 set PATH=%PATH%;%INT%\cygwin\cygwin64\bin
-REM set PATH=%INT%\gnuplot\gnuplot\bin;%PATH%
+set PATH=%INT%\gnuplot\gnuplot\bin;%PATH%
 
 REM perl.exe %PLOT% %1
 start %INT%\cygwin\cygwin64\bin\run.exe /usr/bin/perl.exe %PLOT% %1
